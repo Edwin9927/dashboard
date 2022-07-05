2c09cef6285892fcc4938d581f9563f8ec758ac8
Edwin Manzano <edwinmanzanoquinzo@gmail.com>
2022-07-04 22:21:01 -0500

    Dashboard correccion de archivos

--- src/index.js
+++ src/index.js
@@ -1,14 +1,82 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+*/
+// i18n
+import './locales/i18n';
 
+// highlight
+import './utils/highlight';
+
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+// lightbox import 'react-image-lightbox/style.css';
+import 'react-18-image-lightbox'
+// map
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+// editorimport 'react-quill/dist/quill.snow.css';
+import '@flexocarpius/react-quill-18/dist/react-quill'
+
+// slick-carousel
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+// lazy image
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
+import 'react-lazy-load-image-component/src/effects/black-and-white.css';
+
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+// @mui
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+// redux
+import { store, persistor } from './redux/store';
+// contexts
+import { SettingsProvider } from './contexts/SettingsContext';
+import { CollapseDrawerProvider } from './contexts/CollapseDrawerContext';
+
+// Check our docs
+// https://docs-minimals.vercel.app/authentication/ts-version
+
+import { AuthProvider } from './contexts/JWTContext';
+// import { AuthProvider } from './contexts/Auth0Context';
+// import { AuthProvider } from './contexts/FirebaseContext';
+// import { AuthProvider } from './contexts/AwsCognitoContext';
+
+//
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
+// ----------------------------------------------------------------------
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider>
+    <HelmetProvider>
+      <ReduxProvider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <SettingsProvider>
+              <CollapseDrawerProvider>
+                <BrowserRouter>
+                  <App />
+                </BrowserRouter>
+              </CollapseDrawerProvider>
+            </SettingsProvider>
+          </LocalizationProvider>
+        </PersistGate>
+      </ReduxProvider>
+    </HelmetProvider>
+  </AuthProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
