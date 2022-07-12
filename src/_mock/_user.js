8db6e63912f4e18c687d2313254edbde068fe693
Edwin Manzano <edwinmanzanoquinzo@gmail.com>
2022-07-12 11:01:30 -0500

    Fusion exitosa

--- src/_mock/_user.js
+++ src/_mock/_user.js
@@ -83,22 +83,6 @@ export const _userFeeds = [...Array(3)].map((_, index) => ({
   ],
 }));
 
-export const _userCards = [...Array(24)].map((_, index) => ({
-  id: _mock.id(index),
-  avatarUrl: _mock.image.avatar(index),
-  cover: _mock.image.cover(index),
-  name: _mock.name.fullName(index),
-  follower: randomNumberRange(999, 99999),
-  following: randomNumberRange(999, 99999),
-  totalPost: randomNumberRange(999, 99999),
-  position: _mock.role(index),
-}));
-
-export const _userPayment = [...Array(2)].map((_, index) => ({
-  id: _mock.id(index),
-  cardNumber: ['**** **** **** 1234', '**** **** **** 5678', '**** **** **** 7878'][index],
-  cardType: ['master_card', 'visa', 'master_card'][index],
-}));
 
 export const _userAddressBook = [...Array(4)].map((_, index) => ({
   id: _mock.id(index),
